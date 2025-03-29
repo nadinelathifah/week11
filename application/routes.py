dd2754a7181b5d61debcd39ae34c15c46efedd1d
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-03-29 10:23:47 +0300

    ADDED: 3 new templates for student halls section

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,6 @@ import os
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title_head='Powerpuff University', title_body='Powerpuff University', subtitle='explore your ambitions', img="static/images/university/campus1.png")
+    return render_template('home.html', title_head='Powerpuff University', title_body='Powerpuff University', subtitle='★ explore your ambitions ★', img="static/images/university/campus1.png")
 
 
