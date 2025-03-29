b0f4412454d0fdba3d94f4121e58c68146a1109e
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-03-29 03:27:33 +0300

    ADDED: navbar on layout.html file; images for the university on images/university directory; instantiated flask app object in __init__.py; requirements; and added home route to routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,2 +1,12 @@
-from flask import render_template
+from flask import render_template, url_for, request, redirect, session
+
 from application import app
+import os
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title_head='Powerpuff University', title_body='Powerpuff University')
+
+
