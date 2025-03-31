48702a33f9b6e77fa01286799571ccc152c30e3f
Daliya Stanley <daliya.kurien@gmail.com>
2025-03-31 12:23:59 +0100

    Added the data access trial items

--- application/dataccess.py
+++ application/dataccess.py
@@ -1,18 +1,21 @@
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="signupDB"
+  database="signupdb"
 )
+
+
 def main():
     print(mydb)
 
     cursor = mydb.cursor()
 
-    sql = "INSERT INTO person (firstname, lastname, email ) VALUES (%s, %s, %s)"
-    val = ("Fred", "Flintstone", "abc@m.com")
+    sql = "INSERT INTO sign_up_ppf (firstname, lastname, email, SocietyMembershipID) VALUES (%s, %s, %s,%s)"
+    val = ('Ross', 'Geller', 'gh@hj.com', 'Eat and Retreat Society');
     cursor.execute(sql, val)
 
     mydb.commit()
@@ -20,15 +23,40 @@ def main():
     print(cursor.rowcount, "record inserted.")
 
 
-def get_db_connection():
+def get_ppgirls_connection():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
-        database="signupDB"
+        database="signupdb"
     )
+
     return mydb
 
+
+def add_member(firstname, lastname, email,societymembershipID ):
+    conn = get_ppgirls_connection()
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO person (firstname, lastname, email, SocietyMembershipID) VALUES (%s, %s, %s, %s)"
+    val = (firstname, lastname, email, societymembershipID)
+    cursor.execute(sql, val)
+
+    conn.commit()
+
+
+def get_member():
+    conn = get_ppgirls_connection()
+    cursor = conn.cursor()
+
+    sql = "Select ID, Firstname, Lastname, email and SocietyName from person"
+    cursor.execute(sql)
+
+    result_set = cursor.fetchall()
+    member_list = []
+    for member in result_set:
+        member.append({'ID': member[0], 'Firstname': member[1], 'Lastname': member[2], 'email': member[3],'Society Name': member[4] })
+    return member_list
 
 
 if __name__ == "__main__":
